0cb986c3e918bdf031838bbc2bc22d9b15a0f054
Kaloyy-Ortiaga <90772416+Kaloyy-Ortiaga@users.noreply.github.com>
2024-02-09 00:55:43 +0800

    Start

---
+++ laravel/resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+</body>
+
+</html>
