18319e60565e02f1d6349c1326f715567608153e
Patchiiie <149369534+Patchiiie@users.noreply.github.com>
2024-03-21 10:42:26 +0800

    version update and database migrations

---
+++ laravel/database/migrations/2024_03_20_093345_create_purchase_requests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchase_requests', function (Blueprint $table) {
+            $table->id(); // Creates an auto-incrementing primary key column 'id'
+            $table->date('date_created');
+            $table->integer('pr_no')->unsigned()->unique();
+            $table->enum('department', ['CHASS', 'CET', 'CN', 'CS', 'CISTM']);
+            $table->enum('status', ['denied', 'approved', 'pending']);
+            $table->timestamps(); // Adds 'created_at' and 'updated_at' columns for timestamps
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchase_requests');
+    }
+};
---
+++ laravel/database/migrations/2024_03_20_093707_create_create_prs_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('create_prs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('stock_no')->unsigned();
+            $table->string('unit')->nullable();
+            $table->string('item_description');
+            $table->integer('quantity');
+            $table->decimal('unit_cost', 10, 2);
+            $table->decimal('amount', 10, 2);
+            $table->enum('department', ['CHASS', 'CET', 'CN', 'CS', 'CISTM']);
+            $table->bigInteger('pr_no')->unsigned();
+            $table->date('pr_date');
+            $table->string('section');
+            $table->integer('sai_no')->unsigned();
+            $table->date('sai_date');
+            $table->string('requested');
+            $table->string('designation');
+            $table->string('purpose');
+            $table->timestamps();
+            
+            // Foreign key constraint
+            $table->foreign('pr_no')->references('id')->on('create_pr')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('create_prs');
+    }
+};
