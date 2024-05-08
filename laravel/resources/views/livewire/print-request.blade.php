4b8c65bebc374adb8b711567d34831fad4299158
Patchiiie <149369534+Patchiiie@users.noreply.github.com>
2024-05-08 15:50:34 +0800

    Update print-request.blade.php

--- laravel/resources/views/livewire/print-request.blade.php
+++ laravel/resources/views/livewire/print-request.blade.php
@@ -17,7 +17,7 @@
                                 PRINT
                             </button>
                             <button 
-                                class=" w-[74px] h-9 mt-10 mr-2 justify-center font-medium text-white rounded-md shadow bg-black">
+                                class=" w-[74px] h-9 mt-10 mr-2 justify-center font-medium text-white rounded-md shadow bg-black" onclick="window.location.href='{{ route('purchase-order') }}'">
                                 BACK
                             </button>
                         </div>
