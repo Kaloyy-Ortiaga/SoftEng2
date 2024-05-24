6fbef7e62c782ad2457c19166ddc12210a859230
Kaloyy-Ortiaga <90772416+Kaloyy-Ortiaga@users.noreply.github.com>
2024-05-25 02:17:37 +0800

    Update purchase-order-admin.blade.php

--- laravel/resources/views/livewire/purchase-order-admin.blade.php
+++ laravel/resources/views/livewire/purchase-order-admin.blade.php
@@ -17,7 +17,7 @@
                                     @include('livewire.search-bar')
                                 </div>
                                 <button onclick="window.location.href='{{ route('purchase-order-view') }}'" class="justify-center h-10 mr-6 px-4 py-2 mt-3.5 font-medium text-white rounded-md shadow bg-zinc-900 w-full md:w-auto">
-                                    CREATE PURCHASE REQUEST
+                                    VIEW PURCHASE ORDER
                                 </button>
                             </div>
                             <div class="overflow-x-auto">
