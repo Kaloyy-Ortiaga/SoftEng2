b0186b3f1ed80581ae9bf963bf223ec84bed72f8
Kaloyy-Ortiaga <90772416+Kaloyy-Ortiaga@users.noreply.github.com>
2024-05-28 11:52:44 +0800

    Update admin-po-table.blade.php

--- laravel/resources/views/livewire/includes/admin/admin-po-table.blade.php
+++ laravel/resources/views/livewire/includes/admin/admin-po-table.blade.php
@@ -1,26 +1,42 @@
 <div class="flex flex-col mt-6 overflow-x-auto -mb-9">
-    <table class="w-full mt-6 mb-8 text-sm text-left text-black border-2 border-black dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-            <tr>
-                <th scope="col" class="w-2/12 px-4 py-3 border-r border-black">Item no.</th>
-                <th scope="col" class="w-1/12 px-4 py-3 border-r border-black">Quantity</th>
-                <th scope="col" class="w-1/12 px-4 py-3 border-r border-black">Unit</th>
-                <th scope="col" class="w-3/12 px-4 py-3 border-r border-black">Item Description</th>
-                <th scope="col" class="w-2/12 px-4 py-3 border-r border-black">Unit Cost</th>
-                <th scope="col" class="w-2/12 px-4 py-3">Amount</th>
-            </tr>
-        </thead>
+     <table class="w-full mt-6 mb-8 text-sm text-left text-black border-2 border-black dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                    <tr>
+                        <th scope="col" class="w-2/12 px-4 py-3 border-r border-black">Stock No.</th>
+                        <th scope="col" class="w-1/12 px-4 py-3 border-r border-black">Unit</th>
+                        <th scope="col" class="w-3/12 px-4 py-3 border-r border-black">Item Description</th>
+                        <th scope="col" class="w-1/12 px-4 py-3 border-r border-black">Quantity</th>
+                        <th scope="col" class="w-2/12 px-4 py-3 border-r border-black">Unit Cost</th>
+                        <th scope="col" class="w-2/12 px-4 py-3">Amount</th>
+                    </tr>
+                </thead>
         <tbody>
             @foreach ($adminPurchaseOrder->prsItems as $item)
             <tr class="text-black border-b border-black">
-                <td class="px-4 py-2 border-r border-black">{{ $item->item_no }}</td>
-                <td class="px-4 py-2 border-r border-black">{{ $item->quantity }}</td>
-                <td class="px-4 py-2 border-r border-black">{{ $item->unit }}</td>
-                <td class="px-4 py-2 border-r border-black">{{ $item->item_description }}</td>
-                <td class="px-4 py-2 border-r border-black">₱ {{ number_format($item->unit_cost, 2, '.', ',') }}</td>
-                <td class="px-4 py-2">₱ {{ number_format($item->amount, 2, '.', ',') }}</td>
+                <th class="px-4 py-2 border-r border-black">
+                    <input type="text" value="{{  $item->item_no }}" class="w-full text-left bg-transparent border-0 focus:outline-none" readonly>
+                </th>
+                <td class="px-4 py-2 border-r border-black">
+                    <input type="text" value="{{ $item->quantity }}" class="w-full text-left bg-transparent border-0 focus:outline-none" readonly>
+                </td>
+                <td class="px-4 py-2 border-r border-black">
+                    <input type="text" value="{{ $item->unit }}" class="w-full text-left bg-transparent border-0 focus:outline-none" readonly>
+                </td>
+                <td class="px-4 py-2 border-r border-black">
+                    <input type="text" value="{{ $item->item_description }}" class="w-full text-left bg-transparent border-0 focus:outline-none" readonly>
+                </td>
+                <td class="px-4 py-2 border-r border-black">
+                    <input type="text" value="₱ {{ number_format($item->unit_cost, 2, '.', ',') }}" class="w-full text-left bg-transparent border-0 focus:outline-none" readonly>
+                </td>
+                <td class="px-4 py-2">
+                    <input type="text" value="₱ {{ number_format($item->amount, 2, '.', ',') }}" class="w-full text-left bg-transparent border-0 focus:outline-none" readonly>
+                </td>
             </tr>
             @endforeach
+            <tr class="border-b border-black">
+                <td colspan="5" class="px-4 py-2 font-bold text-right border-r border-black">Total:</td>
+                <td class="px-4 py-2 font-bold">₱ {{ number_format($adminPurchaseOrder->prsItems->sum('amount'), 2, '.', ',') }}</td>
+            </tr>
         </tbody>
     </table>
 </div>
